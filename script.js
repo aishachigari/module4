496dfe78603c39302cccacc307ff5238e6742409
aishachigari <159912432+aishachigari@users.noreply.github.com>
2024-02-14 01:06:19 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+// Array of names
+var names = ["John", "Jane", "Jack", "Jim", "Jill", "Joe", "Mary"];
 
+// Loop over the array
+for (var i = 0; i < names.length; i++) {
+    // Get the current name
+    var name = names[i];
+
+    // Check if the name starts with 'j' or 'J'
+    if (name.charAt(0).toLowerCase() === 'j') {
+        console.log("Goodbye " + name);
+    } else {
+        console.log("Hello " + name);
+    }
+}
